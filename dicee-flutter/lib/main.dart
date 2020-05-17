e5331cffeb76493128eac6c37cfafabdfac76728
Pranava <pranava.r@lenskart.in>
2020-05-17 22:19:46 +0530

    Dice app

---
+++ dicee-flutter/lib/main.dart
@@ -0,0 +1,79 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                role();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                role();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void role() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+}
+
+// ignore: must_be_immutable
+//class Dice extends StatelessWidget {
+//  int number = 1;
+//  VoidCallback onPressed;
+//  Dice(this.number, this.onPressed);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Expanded(
+//      child: FlatButton(
+//        onPressed: () {
+//          onPressed();
+//        },
+//        child: Image.asset('images/dice$number.png'),
+//      ),
+//    );
+//  }
+//}
---
+++ useful-links.md
@@ -0,0 +1,7 @@
+https://appicon.co/
+https://www.canva.com/
+https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e
+https://fonts.google.com/
+https://www.materialpalette.com/
+https://github.com/londonappbrewery/
+
